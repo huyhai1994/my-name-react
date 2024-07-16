d077ecb79cb62e26504a7f3d18869cebf25332d8
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 04:58:21 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+/*TODO:Sử dụng const để tạo biến name mang giá trị là “Your name”
+*/
+const name = "Xin chào các bạn tôi là Hải đây";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+/*TODO:    Import biến vừa tạo vào hàm ReactDOM.render() để render element ra UI
+*/
+/*TODO:    Khai báo biến root và khởi tạo với ReactDOM.createRoot()
+*/
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    /*TODO:Sử dụng hàm React.createElement để khởi tạo element mang giá trị hiển thị tên của bạn*/
+    React.createElement("h1", {style: {textAlign: "center"}}, name)
+    /*TODO:    Truyền element ở Bước 4 vào hàm root.render()
+*/
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
